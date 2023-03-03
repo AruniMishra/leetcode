c26f6040f9b80d9fb8ad2b81abc4f11071128bcb
Aruni Mishra <arunimishramsit@gmail.com>
2023-03-03 19:20:03 +0530

    LeetCode 0443- working also at leetcode

--- LeetcodeProblems/src/StringCompression0443.java
+++ LeetcodeProblems/src/StringCompression0443.java
@@ -1,10 +1,17 @@
+import java.util.Arrays;
+
 public class StringCompression0443 {
 
     public static void main(String[] args) {
 
         char[] chars = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+        char[] chars2 = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
 
         new StringCompression0443().compress(chars);
+
+        System.out.println("---------------");
+
+        new StringCompression0443().compress2(chars2);
 
     }
 
@@ -12,12 +19,10 @@ public class StringCompression0443 {
         int index = 0;
         int counter = 0;
         int length = chars.length;
-        boolean counterReset = true;
         StringBuilder sBuilder = new StringBuilder();
         for (char c : chars) {
-            if (index < length - 1 && c == chars[index + 1]) {
+            if (index < length - 1 && c == chars[++index]) {
                 counter++;
-                index++;
                 continue;
             } else {
                 if (++counter == 1) {
@@ -25,27 +30,59 @@ public class StringCompression0443 {
                 } else {
                     sBuilder.append(c).append(counter);
                 }
-                counterReset = true;
             }
-            if (counterReset) {
-                counter = 0;
-            }
-            index++;
+            counter = 0;
         }
-        String s = sBuilder.toString();
-
-
-        System.out.println(s);
-
-
-        chars = s.toCharArray();
-        for (int i = 0; i < s.length(); i++) {
-            chars[i] = s.charAt(i);
+        String compressed = sBuilder.toString();
+        //chars = compressed.toCharArray();
+        for (int i = 0; i < compressed.length(); i++) {
+            chars[i] = compressed.charAt(i);
         }
 
-        System.out.println(chars);
+        System.out.println("compressed: " + compressed);
+        System.out.println(compressed.length());
+        System.out.println("chars: " + String.valueOf(chars));
         System.out.println(chars.length);
 
-        return chars.length;
+        return compressed.length();
+    }
+
+    public int compress2(char[] chars) {
+        if (chars.length == 1) {
+            return 1;
+        }
+        StringBuilder result = new StringBuilder();
+        char current = chars[0];
+        int sum = 0;
+
+        for (char aChar : chars) {
+            if (aChar == current) {
+                sum++;
+            } else {
+                if (sum == 1) {
+                    result.append(current);
+                } else {
+                    result.append(current).append(sum);
+                }
+                sum = 1;
+                current = aChar;
+            }
+        }
+
+        if (sum == 1) {
+            result.append(current);
+        } else {
+            result.append(current).append(sum);
+        }
+        String compressed = result.toString();
+        for (int i = 0; i < compressed.length(); i++) {
+            chars[i] = compressed.charAt(i);
+        }
+        System.out.println("compressed: " + compressed);
+        System.out.println(compressed.length());
+        System.out.println("chars: " + String.valueOf(chars));
+        System.out.println(chars.length);
+
+        return compressed.length();
     }
 }
